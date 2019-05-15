32db7a97b0abb146d8662b7ac257f656551ec464
209347 <209347@edu.p.lodz.pl>
2019-05-15 08:47:33 +0200

    Added MockClock

---
+++ src/main/java/edu/iis/mto/time/MockClock.java
@@ -0,0 +1,33 @@
+package edu.iis.mto.time;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+
+public class MockClock extends Clock {
+
+    private long clockOffset;
+
+    public void setClockOffset(long clockOffset) {
+        this.clockOffset = clockOffset;
+    }
+
+    @Override
+    public ZoneId getZone() {
+        return ZoneId.systemDefault();
+    }
+
+    @Override
+    public Clock withZone(ZoneId zone) {
+        return Clock.fixed(instant(), zone);
+    }
+
+    @Override
+    public Instant instant() {
+        return Instant.now();
+    }
+
+    public Instant instantWithOffset() {
+        return instant().plusSeconds(clockOffset);
+    }
+}
